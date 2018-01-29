4a370eef2319cfca3e4af764adbfe1b5aed5e49d
张展亮 <zzlwte@email.com>
2018-01-29 14:51:41 +0800

    message

--- src/pages/operator/join/form.vue
+++ src/pages/operator/join/form.vue
@@ -4,7 +4,9 @@
 
 <template>
     <div class="pb60">
-        <div class="w100 flex-wrp flex-center size38 color2" :style="{height: rem(800)}">海报编辑中。。</div>
+        <div class="w100 flex-wrp flex-center size38 color2">
+          <img width="100%" src="~/assets/img/web/2.png" class="bg-cover"/>
+        </div>
         <base-title title="申请入住服务商"/>
         <group label-width="2rem" label-margin-right="2em" label-align="left" gutter="0">
             <x-input title="平台名称" placeholder="请输入平台名称" required v-model="title"/>
